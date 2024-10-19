b11df471132a1dfd65302c61d79760928b77d413
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2024-10-19 13:29:48 +0530

    Add protected routes for admin and student

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,30 @@
 import { Router } from "express";
+import { ApiResponse } from "../utils/ApiResponse.js";
 import { loginUser, registerUser } from "../controllers/user.controllers.js";
+import {
+    authMiddleware,
+    isAdmin,
+    isStudent,
+} from "../middlewares/auth.middlewares.js";
 
 const router = Router();
 
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
+
+
+
+// protected Routes
+router.route("/student").get(authMiddleware, isStudent, (_, res) => {
+    return res
+        .status(200)
+        .json(new ApiResponse(200, "Welcome to protected route for Student"));
+});
+
+router.route("/admin").get(authMiddleware, isAdmin, (_, res) => {
+    return res
+        .status(200)
+        .json(new ApiResponse(200, "Welcome to protected route of Admin"));
+});
 
 export default router;
